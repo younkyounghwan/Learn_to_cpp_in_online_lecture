e6933cf37f0b793048cb462d1b0328be56832053
Kyounghwan Youn <youn7117@naver.com>
2021-02-14 21:39:43 +0900

    21-02-14

---
+++ learn to cpp/cpp 4.3/cpp 4.3/ambiguity_of_using.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+namespace a
+{
+	int my_var(10);
+}
+namespace b
+{
+	int my_var(20);
+}
+using namespace std;
+//using std::cout;
+//using std::endl;
+
+using namespace a;
+using namespace b;
+int main()
+{
+	
+	cout << "Hello" << endl;
+	//cout << my_var << endl; ambiguity
+	return 0;
+
+}
