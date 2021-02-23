931bce7cb0fae98d86aefcf2ef6d4e4631d9578d
Kyounghwan Youn <youn7117@naver.com>
2021-02-24 00:50:23 +0900

    21-02-24

---
+++ learn to cpp/cpp 5.1/cpp 5.1/control flow.cpp
@@ -0,0 +1,22 @@
+/*
+Halt
+jump 
+conditional braches
+loops
+exceptions
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	using namespace std;
+
+	cout << "I love you " << endl;
+
+	cout << "I love you, too " << endl;
+
+	return 0;
+	exit(0); //무조건 여기서 끝내!!!
+}
